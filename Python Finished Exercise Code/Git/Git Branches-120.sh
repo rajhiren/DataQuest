32bcc77317807c84e4516d5f2491b886236cc4d6
raj hiren <hiren.raj@icloud.com>
2019-10-21 11:55:31 +1100

    Add files via upload

---
+++ Python Finished Exercise Code/Git/Git Branches-120.sh
@@ -0,0 +1,35 @@
+## 1. Introduction to Git Branches ##
+
+/home/dq/chatbot$ git checkout -b more-speech
+
+## 2. Switching Branches ##
+
+/home/dq/chatbot$ git branch
+
+## 3. Pushing a Branch to a Remote ##
+
+/home/dq/chatbot$ git push
+
+## 4. Merging Branches ##
+
+/home/dq/chatbot$ git push origin master
+
+## 5. Deleting Branches ##
+
+/home/dq/chatbot$ git branch -d more-speech
+
+## 6. Checking Out Branches From the Remote ##
+
+/home/dq/chatbot$ cd ..
+
+## 7. Finding Differences Across Branches ##
+
+/home/dq/chatbot$ git diff
+
+## 8. Branch Naming Conventions ##
+
+/home/dq/chatbot$ git push origin feature/random-messages
+
+## 9. Branch History ##
+
+/home/dq/chatbot$ git checkout feature/random-messages
